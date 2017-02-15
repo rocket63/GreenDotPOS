8475e96b619c028596059dce8824e685f7eb53d0
rocket63 <robbtuk@hotmail.com>
2017-02-15 11:04:15 -0500

    Initial upload

---
+++ src/PointOfSale.java
@@ -0,0 +1,47 @@
+
+public class PointOfSale {
+
+    private String name;
+    private String category;
+    private String description;
+    private double price;
+
+    public PointOfSale(String name, String category, String description, double price) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
