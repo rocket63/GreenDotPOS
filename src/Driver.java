fa59aaf40937625685795f90c7fce2e53d2a84ac
rocket63 <robbtuk@hotmail.com>
2017-02-16 13:46:30 -0500

    Your order

--- src/Driver.java
+++ src/Driver.java
@@ -17,18 +17,21 @@ public class Driver extends Main {
         int itemsTotal = 0;
         double grandTotal = 0.00;
         String paymentMethod = "";
-
+        ArrayList<PointOfSale> userOrder = new ArrayList<>();
         while (true) {
 
+            System.out.println("Welcome to Green Dot Stables!");
+            System.out.println("Our menu: ");
+            System.out.println();
             //Printing out menu
             pointOfSaleList = MenuFromFile.readTextFromFile("menuItems.txt");
             for (int i = 0; i < pointOfSaleList.size(); i++) {
                 if (i < 9)
                     System.out.println((i + 1) + ".  " + pointOfSaleList.get(i).toString());
-            else {
-        System.out.println((i + 1) + ". " + pointOfSaleList.get(i).toString());
-    }
-}
+                else {
+                    System.out.println((i + 1) + ". " + pointOfSaleList.get(i).toString());
+                }
+            }
             //TODO format arraylist
 
             //Gets user order and assigns choices to new arraylist
@@ -37,9 +40,12 @@ public class Driver extends Main {
             int userChoice = Validator.getInt(scan, "Enter item # you want to order: ", 1, 12);
             int numItems = Validator.getInt(scan, "How many of item # " + userChoice + " would you like: ");
 
-            ArrayList<PointOfSale> userOrder = new ArrayList<>();
 
-            userOrder.add(pointOfSaleList.get(userChoice - 1));
+
+
+            for (int i = 0; i < numItems; i++) {
+                userOrder.add(pointOfSaleList.get(userChoice - 1));
+            }
 
             //Get price from pointOfSale and using in userOrder
             if ((pointOfSaleList.get(userChoice - 1).getName().equalsIgnoreCase("Cheeseburger")) ||
@@ -93,6 +99,9 @@ public class Driver extends Main {
         System.out.println("RECEIPT");
         System.out.println("========");
         System.out.println("Your order is: ");
+        for (int i = 0; i < userOrder.size(); i++) {
+            System.out.println(userOrder.get(i));
+        }
         //TODO print names of yourOrder
         receiptInfo(priceTotal, itemsTotal);
         System.out.printf(paymentMethod);
--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<PointOfSale> pointOfSaleList = new ArrayList<>();
+
 
         Driver.driver();
 
-        pointOfSaleList = MenuFromFile.readTextFromFile("menuItems.txt");
-        for (int i = 0; i < pointOfSaleList.size(); i++) {
-            System.out.println(pointOfSaleList.get(i).toString());
-        }
+
+
 
     }
 }
